f2662df103329a86af682cb929f5dfbe7e767171
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-30 20:12:31 +0200

    update script

--- starter/script.js
+++ starter/script.js
@@ -2,9 +2,17 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
+  const firstName = 'Steven';
   function printAge() {
     const output = `You are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `oh, and you 're a millenial ${firstName}`;
+      console.log(str);
+    }
+    console.log(millenial);
   }
   printAge();
   return age;
