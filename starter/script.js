0ca7271df2a5badbe0fd4013750855f1d0843b00
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-05-04 13:23:10 +0200

    maj script

--- starter/script.js
+++ starter/script.js
@@ -1,9 +1,23 @@
 'use strict';
 
-console.log(me);
-console.log(job);
-console.log(year);
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
 
-var me = 'Nicolas';
-let job = 'development';
-const year = 1979;
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Exemple de ce qu'il ne faut pas faire
+if(!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
